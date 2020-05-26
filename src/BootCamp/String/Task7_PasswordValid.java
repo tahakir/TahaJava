13dfef6ca149cb7cb336a6a7b1ed201f8f579e6e
tahakir <tahakir.gmail.com>
2020-05-26 15:04:05 -0400

    bootcamp day 1

--- src/BootCamp/String/Task7_PasswordValid.java
+++ src/BootCamp/String/Task7_PasswordValid.java
@@ -2,6 +2,28 @@ package BootCamp.String;
 
 public class Task7_PasswordValid {
     public static void main(String[] args) {
+/**
+ * 1. Password MUST be at least 8 characters
+ * 2. Password should at least contain one upper case letter
+ * 3. Password should at least contain one lowercase letter
+ * 4. Password should at least contain one special characters
+ * 5. Password should at least contain a digit
+ */
+        String password = "a?G6jdsaja";
+
+        boolean length=password.length()>=8;
+        boolean lower=password.matches(".*[a-z].*");
+        boolean upper=password.matches(".*[A-Z].*");
+        boolean special=password.matches(".*[!@.,#$?].*");
+        boolean number=password.matches(".*[0-9].*");
+
+        boolean valid=length&&lower&&upper&&special&&number;
+
+        if (valid){
+            System.out.println("Password is valid");
+        }else{
+            System.out.println("Not valid Password");
+        }
 
     }
 }
